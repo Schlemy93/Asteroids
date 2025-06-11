a3f2523f6c47269000670559198be2ac1cf21cfc
Schlemy <kaebird93@gmail.com>
2025-06-11 17:06:29 -0500

    We have rotation!

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+print("Attempting to import pygame...")
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 def main():
 
 	pygame.init()
@@ -10,13 +13,24 @@ def main():
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	clock = pygame.time.Clock()
+	dt = 0
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /  2)
+
 	while True:
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+	
+		dt = clock.tick(60) / 1000
 
+		player.update(dt)
+
+	
 
 
 
