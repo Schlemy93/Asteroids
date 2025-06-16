8fa38d108e1f2b9aab9ff0e46cebc332cac2ac08
Schlemy <kaebird93@gmail.com>
2025-06-16 12:40:55 -0500

    game over

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-print("Attempting to import pygame...")
+import sys
 import pygame
 from constants import *
 from player import *
 from circleshape import *
+from asteroidfield import *
 def main():
 
 	pygame.init()
@@ -15,11 +16,21 @@ def main():
 
 	clock = pygame.time.Clock()
 	dt = 0
+	asteroids = pygame.sprite.Group()
+	asteroid_field = pygame.sprite.Group()
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	AsteroidField.containers = (updatable,)
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /  2)
-
+	asteroidfield = AsteroidField()
 	while True:
 		screen.fill("black")
-		player.draw(screen)
+		
+		for draw in drawable:
+			draw.draw(screen)
+
 		pygame.display.flip()
 
 		for event in pygame.event.get():
@@ -28,9 +39,13 @@ def main():
 	
 		dt = clock.tick(60) / 1000
 
-		player.update(dt)
+		updatable.update(dt)
 
-	
+		for asteroid in asteroids:
+			if asteroid.collisioncheck(player):
+				print("Game Over!")
+				sys.exit()
+				pass
 
 
 
